65a224e2a3c5f10da8823e9032545e340ca5229c
goodzsq <goodzsq@gmail.com>
2011-12-06 21:21:07 +0800

    integrate phpDataMapper Signed-off-by: goodzsq <goodzsq@gmail.com>

---
+++ ggphp_core/phpDataMapper/controller.php
@@ -0,0 +1,9 @@
+<?php
+
+class phpDataMapper_controller{
+
+	function doIndex(){
+		return html("Integate phpDataMapper: <a href='http://phpdatamapper.com/'>http://phpdatamapper.com/</a><br>git clone git://github.com/vlucas/phpDataMapper.git", 'phpDataMapper');
+	}
+
+}
