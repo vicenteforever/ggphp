acc196ea66d095593e8ec13fc70ef054e0e99628
goodzsq <goodzsq@gmail.com>
2012-02-16 11:03:20 +0800

    mysql

--- ggphp_core/database/mysql/adapter.php
+++ ggphp_core/database/mysql/adapter.php
@@ -10,36 +10,68 @@ class database_mysql_adapter {
     private $_dbh;
 
     function __construct($server, $username, $password, $dbname, $charset) {
-        $this->connect($server, $username, $password, $dbname, $charset);
+        $this->init($server, $username, $password, $dbname, $charset);
     }
 
-    function dbh() {
-        return $this->_dbh;
-    }
-
-    private function connect($server, $username, $password, $dbname, $charset) {
+    private function init($server, $username, $password, $dbname, $charset) {
         $dbh = @mysql_connect($server, $username, $password);
         if (!$dbh) {
             throw new Exception("database fail");
         }
         mysql_select_db($dbname);
         if (!empty($charset)) {
-            mysql_query("SET NAMES {$charset}");
+            mysql_query("SET NAMES {$charset}", $dbh);
         }
         $this->_dbh = $dbh;
     }
-
-    function query($sql) {
-        return new database_mysql_result(mysql_query($sql));
+    
+    /**
+     * 执行SQL查询语句，发生错误时扔出异常
+     * @param string $sql
+     * @return resource 
+     */
+    function query($sql){
+        $sql = mysql_escape_string($sql);
+        $rs = mysql_query($sql, $this->_dbh);
+        if($rs === false){
+            throw new Exception('mysqlerror' . mysql_error($this->_dbh));
+        }
+        return $rs;
+    }
+    
+    /**
+     * 执行非查询语句，返回影响的行数
+     * @param string $sql
+     * @return integer 
+     */
+    function exec($sql){
+        $this->query($sql);
+        return mysql_affected_rows();
     }
 
-    function cacheQuery($sql) {
+    /**
+     * 执行查询语句并返回结果集
+     * @param string $sql
+     * @return database_mysql_result 
+     */
+    function result($sql) {
+        $sql = mysql_escape_string($sql);
+        $rs = $this->execSql($sql);
+        return new database_mysql_result($rs);
+    }
+
+    /**
+     * 执行查询语句并缓存结果集
+     * @staticvar array $result
+     * @param string $sql
+     * @return database_mysql_result 
+     */
+    function cacheResult($sql) {
         static $result;
         if (!isset($result[$sql])) {
-            $result[$sql] = $this->query($sql);
+            $result[$sql] = $this->result($sql);
         }
         return $result[$sql];
     }
 
 }
-
--- ggphp_core/database/mysql/result.php
+++ ggphp_core/database/mysql/result.php
@@ -1,12 +1,69 @@
 <?php
 
-/**
- * 查询结果集
- * @package database
- * @author goodzsq@gmail.com
- */
-class database_mysql_result {
-    //put your code here
+class database_mysql_result implements Countable, Iterator, ArrayAccess {
+
+    private $_result;
+    private $_numRows = 0; //查询结果集有多少行  
+    private $_iterPos = 0; //当前迭代器遍历到的位置
+
+    public function __construct($result) {
+        $this->_result = $result;
+        $this->_numRows = mysql_num_rows($this->_result);
+    }
+
+    public function count() {
+        return $this->_numRows;
+    }
+
+    public function fetchAll() {
+        if($this->_numRows<1){
+            return array();
+        }
+        mysql_data_seek($this->_result, 0);
+        $data = array();
+        while ($row = mysql_fetch_assoc($this->_result)) {
+            $data[] = $row;
+        }
+        return $data;
+    }
+
+    public function current() {
+        return mysql_fetch_assoc($this->_result);
+    }
+
+    public function key() {
+        return $this->_iterPos;
+    }
+
+    public function next() {
+        $this->_iterPos++;
+    }
+
+    public function rewind() {
+        $this->_iterPos = 0;
+        mysql_data_seek($this->_result, 0);
+    }
+
+    public function valid() {
+        return $this->_iterPos > -1 && $this->_iterPos < $this->_numRows;
+    }
+
+    public function offsetExists($offset) {
+        return $offset > -1 && $offset < $this->_numRows;
+    }
+
+    public function offsetGet($offset) {
+        mysql_data_seek($this->_result, 0);
+        return mysql_fetch_assoc($this->_result);
+    }
+
+    public function offsetSet($offset, $value) {
+        //do nothing
+    }
+
+    public function offsetUnset($offset) {
+        //do nothing
+    }
+
 }
 
-?>
